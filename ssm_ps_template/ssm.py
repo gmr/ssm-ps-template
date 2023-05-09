a23106b188e8cd7ed11a8848f466a332555fb819
Gavin M. Roy <gavinr@aweber.com>
2023-05-09 12:47:11 -0400

    Refactor SSM to support path based dicts

--- ssm_ps_template/ssm.py
+++ ssm_ps_template/ssm.py
@@ -2,6 +2,7 @@ import logging
 import typing
 
 import boto3
+import flatdict
 
 LOGGER = logging.getLogger(__name__)
 
@@ -13,6 +14,7 @@ class ParameterStore:
                  region: typing.Optional[str] = None):
         self._session = boto3.Session(profile_name=profile, region_name=region)
         self._client = self._session.client('ssm')
+        self._ssm = boto3.client('ssm')
 
     def fetch_variables(self,
                         variables: list,
@@ -20,6 +22,9 @@ class ParameterStore:
         # Build the variables
         names = ['/'.join([prefix.rstrip('/'), v])
                  if prefix and not v.startswith('/') else v for v in variables]
+
+        paths = [name for name in names if name.endswith('/')]
+        names = [name for name in names if not name.endswith('/')]
 
         LOGGER.debug('Fetching %r', names)
 
@@ -29,18 +34,44 @@ class ParameterStore:
                 Names=names[:10],
                 WithDecryption=True)
 
-            # Strip the prefix if needed
             for param in response['Parameters']:
-                # LOGGER.debug('Param %r', param)
-                name = param['Name']
-                if name.startswith(prefix) and name not in variables:
-                    name = name[len(prefix):]
-                if param['Type'] == 'StringList':
-                    values[name] = [p for p in param['Value'].split(',')
-                                    if p.strip()]
-                    LOGGER.debug('%s = %r', name, values[name])
-                else:
-                    values[name] = param['Value'].rstrip()
+                values = self.add_parameter(
+                    param, prefix, variables, values)
             names = names[10:]
+
+        path_values = flatdict.FlatDict(delimiter='/')
+        for path in paths:
+            paginator = self._client.get_paginator('get_parameters_by_path')
+            for page in paginator.paginate(
+                    Path=path,
+                    Recursive=True,
+                    WithDecryption=True):
+                for param in page['Parameters']:
+                    LOGGER.debug('Param %r', param)
+                    path_values = self.add_parameter(
+                        param, prefix, variables, path_values)
+        for key, value in path_values.as_dict().items():
+            values[f'{key}/'] = value
+
         LOGGER.debug('Returning %r', values)
+        return values
+
+    @staticmethod
+    def add_parameter(param: dict,
+                      prefix: str,
+                      variables: typing.List[str],
+                      values: typing.Union[dict, flatdict.FlatDict]) \
+            -> dict:
+        """Process a parameter, stripping prefix if needed and coercing
+        StringList to a list of strings.
+
+        """
+        if param['Name'].startswith(prefix) and param['Name'] not in variables:
+            param['Name'] = param['Name'][len(prefix):]
+        if param['Type'] == 'StringList':
+            values[param['Name']] = [p for p in param['Value'].split(',')
+                                     if p.strip()]
+            LOGGER.debug('%s = %r', param['Name'], values[param['Name']])
+        else:
+            values[param['Name']] = param['Value'].rstrip()
         return values
