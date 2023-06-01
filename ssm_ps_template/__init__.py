71fd6e3425fb0afc6d2dc63bd0d2fbb7c237c124
Gavin M. Roy <gavinr@aweber.com>
2023-06-01 17:49:53 -0400

    Fix typo in description

--- ssm_ps_template/__init__.py
+++ ssm_ps_template/__init__.py
@@ -2,6 +2,6 @@
 SSM Parameter Store Template
 ============================
 
-Command line application to render templates with data from SSM Parameter Stores
+Command line application to render templates with data from SSM Parameter Store
 
 """
